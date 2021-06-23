448f3e6abad817e780b8aecf73fa29e731378557
Kaleidot725 <hakodate.katsuragawa.yusuke@gmail.com>
2021-06-23 21:20:37 +0900

    feat: update iOS layout

--- iosApp/iosApp/ContentView.swift
+++ iosApp/iosApp/ContentView.swift
@@ -2,7 +2,6 @@ import SwiftUI
 import shared
 
 struct ContentView: View {
-    let manager: HttpClientManager = HttpClientManager()
     let gistApi : GistApi = GistApi(manager: HttpClientManager(), baseUrl: "https://api.github.com/users/kaleidot725")
 
     @State var gists: String = ""
@@ -12,13 +11,11 @@ struct ContentView: View {
         Button("UPDATE") {
             gistApi.getGists(
                 completionHandler: { (array, error) in
-                    if (array == nil) {
-                        return
+                    if (array != nil) {
+                        let gists = array!.compactMap { $0 as? GistDto }
+                        let gistTexts = gists.compactMap { $0.description() }
+                        self.gists = gistTexts.joined()
                     }
-
-                    let gists = array!.compactMap { $0 as? GistDto }
-                    let gistTexts = gists.compactMap { $0.description() }
-                    self.gists = gistTexts.joined()
                 }
             )
         }
