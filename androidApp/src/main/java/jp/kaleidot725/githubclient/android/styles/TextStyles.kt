31a6a74f7094cb7757bb02630062b9f6a95df9b1
Kaleidot725 <hakodate.katsuragawa.yusuke@gmail.com>
2021-07-04 15:10:55 +0900

    feat: fix gist card

---
+++ androidApp/src/main/java/jp/kaleidot725/githubclient/android/styles/TextStyles.kt
@@ -0,0 +1,102 @@
+package jp.kaleidot725.githubclient.android.styles
+
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.ExperimentalUnitApi
+import androidx.compose.ui.unit.TextUnit
+import androidx.compose.ui.unit.TextUnitType
+import androidx.compose.ui.unit.sp
+
+@OptIn(ExperimentalUnitApi::class)
+object TextStyles {
+    val h1
+        get() = TextStyle(
+            fontSize = 96.sp,
+            fontWeight = FontWeight.Light,
+            letterSpacing = TextUnit(-1.5f, TextUnitType.Sp)
+        )
+
+    val h2
+        get() = TextStyle(
+            fontSize = 60.sp,
+            fontWeight = FontWeight.Light,
+            letterSpacing = TextUnit(-0.5f, TextUnitType.Sp)
+        )
+
+    val h3
+        get() = TextStyle(
+            fontSize = 48.sp,
+            fontWeight = FontWeight.Normal,
+            letterSpacing = TextUnit(0f, TextUnitType.Sp)
+        )
+
+    val h4
+        get() = TextStyle(
+            fontSize = 34.sp,
+            fontWeight = FontWeight.Normal,
+            letterSpacing = TextUnit(0.25f, TextUnitType.Sp)
+        )
+
+    val h5
+        get() = TextStyle(
+            fontSize = 24.sp,
+            fontWeight = FontWeight.Normal,
+            letterSpacing = TextUnit(0f, TextUnitType.Sp)
+        )
+
+    val h6
+        get() = TextStyle(
+            fontSize = 20.sp,
+            fontWeight = FontWeight.Medium,
+            letterSpacing = TextUnit(0.15f, TextUnitType.Sp)
+        )
+
+    val subTitle1
+        get() = TextStyle(
+            fontSize = 16.sp,
+            fontWeight = FontWeight.Normal,
+            letterSpacing = TextUnit(0.15f, TextUnitType.Sp)
+        )
+
+    val subTitle2
+        get() = TextStyle(
+            fontSize = 14.sp,
+            fontWeight = FontWeight.Medium,
+            letterSpacing = TextUnit(0.1f, TextUnitType.Sp)
+        )
+
+    val body1
+        get() = TextStyle(
+            fontSize = 16.sp,
+            fontWeight = FontWeight.Normal,
+            letterSpacing = TextUnit(0.5f, TextUnitType.Sp)
+        )
+
+    val body2
+        get() = TextStyle(
+            fontSize = 14.sp,
+            fontWeight = FontWeight.Normal,
+            letterSpacing = TextUnit(0.25f, TextUnitType.Sp)
+        )
+
+    val button
+        get() = TextStyle(
+            fontSize = 14.sp,
+            fontWeight = FontWeight.Medium,
+            letterSpacing = TextUnit(1.25f, TextUnitType.Sp)
+        )
+
+    val caption
+        get() = TextStyle(
+            fontSize = 12.sp,
+            fontWeight = FontWeight.Normal,
+            letterSpacing = TextUnit(0.4f, TextUnitType.Sp)
+        )
+
+    val overline
+        get() = TextStyle(
+            fontSize = 10.sp,
+            fontWeight = FontWeight.Normal,
+            letterSpacing = TextUnit(1.5f, TextUnitType.Sp)
+        )
+}
--- androidApp/src/main/java/jp/kaleidot725/githubclient/android/view/GistCard.kt
+++ androidApp/src/main/java/jp/kaleidot725/githubclient/android/view/GistCard.kt
@@ -1,5 +1,6 @@
 package jp.kaleidot725.githubclient.android.view
 
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.wrapContentHeight
@@ -9,16 +10,30 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
+import jp.kaleidot725.githubclient.android.styles.TextStyles
 import jp.kaleidot725.githubclient.api.dto.GistDto
 
 @Composable
 fun GistCard(gist: GistDto) {
+    val files = gist.gistFiles
+    val firstFile = files.firstOrNull() ?: return
+    val firstFileName = firstFile.filename
+    val createdAt = gist.createdAt
+
     Card(
         modifier = Modifier
             .fillMaxWidth()
             .wrapContentHeight()
             .padding(8.dp)
     ) {
-        Text(text = gist.gistFiles.firstOrNull()?.filename ?: "EMPTY", color = Color.Black)
+        Column(modifier = Modifier.padding(16.dp)) {
+            Text(text = firstFileName, style = TextStyles.h6, color = Color.Black, maxLines = 2)
+            Text(
+                text = createdAt,
+                style = TextStyles.caption,
+                color = Color.Black,
+                maxLines = 1
+            )
+        }
     }
 }
