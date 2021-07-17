e6f14073b5923d9781e5cdc0ed4e98bf58b99e34
Kaleidot725 <hakodate.katsuragawa.yusuke@gmail.com>
2021-07-17 11:08:03 +0900

    feat: update ContentView

--- iosApp/iosApp/Views/ContentView.swift
+++ iosApp/iosApp/Views/ContentView.swift
@@ -2,32 +2,13 @@ import SwiftUI
 import shared
 
 struct ContentView: View {
-    
-    @State var gists: String = ""
-    
     var body: some View {
         GistPage(viewModel: AppModule.getGistPageViewModel())
-        
-//        Button("UPDATE") {
-//            gistRepositoryNative.getGists(userName: userName).subscribe(
-//                scope: coroutineScope,
-//                onSuccess: { array in
-//                    if (array != nil) {
-//                       let gists = array!.compactMap { $0 as? GistDto }
-//                       let gistTexts = gists.compactMap { $0.description() }
-//                       self.gists = gistTexts.joined()
-//                    }
-//                },
-//                onThrow: { throwable in
-//                    self.gists = throwable.description()
-//                }
-//            )
-//        }
 	}
 }
 
 struct ContentView_Previews: PreviewProvider {
 	static var previews: some View {
-	ContentView()
+        ContentView()
 	}
 }
